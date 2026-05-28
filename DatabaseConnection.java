bd241dae41b97804e5de30a869e1174bc8a5f9f6
PatelJashmin <227741694+PatelJashmin@users.noreply.github.com>
2026-05-28 13:19:54 +0530

    Document database connection and user session logic

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -5,6 +5,8 @@ public class DatabaseConnection {
     private static final String USER = "root";
     private static final String PASSWORD = "your_password"; // Update this
 
+    //built-in getConnection method is in DriverManager class.
+    //and my getConnection method is in DatabaseConnection calss. Hence no conflict.  
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
--- UserSession.java
+++ UserSession.java
@@ -1,3 +1,6 @@
+//pending revision, attempt logs are all based on logged in user. 
+//public static variables are used so that all classes
+//can access the student id without the need to pass them
 public class UserSession {
     public static int currentStudentId;
     public static String currentStudentName;
