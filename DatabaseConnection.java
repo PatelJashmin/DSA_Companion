c83e0ed7686807eb3e394a2249b8b69d076a9a21
PatelJashmin <227741694+PatelJashmin@users.noreply.github.com>
2026-05-29 12:14:17 +0530

    Externalize database credentials using properties file

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,12 +1,28 @@
 import java.sql.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/dsa_tracker";
-    private static final String USER = "root";
-    private static final String PASSWORD = "REMOVED_SECRET"; // Update this
 
-    //built-in getConnection method is in DriverManager class.
-    //and my getConnection method is in DatabaseConnection calss. Hence no conflict.  
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    static {
+        try {
+            Properties props = new Properties();
+            props.load(new FileInputStream("db.properties"));
+
+            URL = props.getProperty("db.url");
+            USER = props.getProperty("db.user");
+            PASSWORD = props.getProperty("db.password");
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load database configuration", e);
+        }
+    }
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
